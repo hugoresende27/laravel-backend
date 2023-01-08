c4d986da5ad7f98641207e76b5f9f5b32d2771ce
hugoresende27 <hugoresende27@gmail.com>
2023-01-08 19:54:05 +0000

    backend using resource JSON

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::paginate(10);
+
+        return UserResource::collection($users);
+    }
 }
